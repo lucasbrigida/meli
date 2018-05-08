55e05f503a8b888b2d08faa27933d88a5989fd77
lucasbrigida <lucasbrigida@gmail.com>
2018-05-08 01:57:24 -0300

    :rocket: /api/items created

---
+++ models/item.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Item {
+    constructor(props = {}) {
+        this.props = props;
+    }
+}
+
+
+module.exports = Item;
---
+++ routes/items.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+var rp = require('request-promise');
+var Item = require('../models/item');
+
+var options = {
+  headers: {
+    'User-Agent': 'MELI Front End-3342184372699420'
+  },
+  json: true
+};
+
+
+/* GET /api/items/:id */
+router.get('/:id', function(req, res, next) {
+  let query = Object.keys(req.query).map(key => key + '=' + req.query[key]).join('&');   
+
+  // get item details
+  let detail = rp(Object.assign(options, {
+    uri: `https://api.mercadolibre.com/items/${req.params.id}?${query}`
+  }));
+
+  // get item description
+  let description = rp(Object.assign(options, {
+    uri: `https://api.mercadolibre.com/items/${req.params.id}/description?${query}`
+  }));
+
+  Promise.all([detail, description])
+  .then(function (resp) {
+    let item = resp.reduce((a, b) => Object.assign(a, b));
+    let itemDetail = {
+      author: {
+        name: '',
+        lastname: ''
+      },
+      item: {
+        id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: parseInt(String(item.price).split('.')[0] || 0),
+          decimals: parseInt(String(item.price).split('.')[1] || 0) 
+        },
+        picture: (item.pictures || [])[0].url,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping,
+        sold_quantity: item.sold_quantity,
+        description: item.text || item.plain_text
+      }
+    };
+    res.status(200).json(itemDetail);
+  })
+  .catch(function (err) {
+    res.status(500).json(err);
+  });
+});
+
+/* GET /api/items */
+router.get('/', function(req, res, next) {
+  let query = Object.keys(req.query).map(key => key + '=' + req.query[key]).join('&');  
+
+  rp(Object.assign(options, {
+    uri: `https://api.mercadolibre.com/sites/MLA/search?${query}`
+  }))
+  .then(function (resp = {}) {
+    let items = (resp.results || []).map(item => {
+      return {
+        id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: parseInt(String(item.price).split('.')[0] || 0),
+          decimals: parseInt(String(item.price).split('.')[1] || 0) 
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping
+      };
+    });
+
+    let listItems = Object.assign({
+      author: {
+        name: '',
+        lastname: ''
+      }
+    }, {items});
+
+    res.status(200).json(listItems);
+  })
+  .catch(function (err) {
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
